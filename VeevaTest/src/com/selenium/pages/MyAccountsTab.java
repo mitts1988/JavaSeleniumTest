9d9b3de3a848fb4e083cdeee6d5f4dad1fd9486b
mitts <mitts1988@outlook.com>
2019-03-10 20:12:18 -0400

    Completed up step 9 of 12

--- VeevaTest/src/com/selenium/pages/MyAccountsTab.java
+++ VeevaTest/src/com/selenium/pages/MyAccountsTab.java
@@ -1,5 +1,8 @@
 package com.selenium.pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +15,9 @@ public class MyAccountsTab {
 	By recordACallButton = By.cssSelector("td[id=\"topButtonRow\"] input.btn[value=\"Record a Call\"]");
 	By formTitle = By.cssSelector("div.bPageTitle.content h1");
 	By recordTypeMenu = By.id("RecordTypeId");
+//	By callDiscussionElements = By.cssSelector(
+//			"div[name=\"zvod_Product_Discussion_vod__c\"] tr [ng-repeat=\"discussion in call.data.Call2_Discussion_vod__r | filter : deleteFilter track by $index\"]");
+//	By callDiscussionProducts = By.cssSelector("select[ng-change=\"updateProduct(discussion)\"]");
 
 	public MyAccountsTab(WebDriver driver) {
 		this.driver = driver;
@@ -48,5 +54,16 @@ public class MyAccountsTab {
 
 	public void selectDetailingPriorityOptions(String strOption) {
 		driver.findElement(By.cssSelector("#vod_detailing input[name=\"" + strOption + "\"]")).click();
+	}
+
+	public List<String> getCallDiscussionProducts() {
+		List<WebElement> eles = driver.findElements(By.xpath("//select[@ng-change='updateProduct(discussion)']"));
+		List<String> products = new ArrayList<String>();
+		for(WebElement e : eles) {
+			Select sel = new Select(e);
+			String product = sel.getFirstSelectedOption().getText();
+			products.add(product);
+		}
+		return products;
 	}
 }
--- VeevaTest/src/com/selenium/tests/TestCallReport.java
+++ VeevaTest/src/com/selenium/tests/TestCallReport.java
@@ -1,5 +1,6 @@
 package com.selenium.tests;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -28,11 +29,13 @@ public class TestCallReport {
 	LoginPage objLogin;
 	MyAccountsTab objAccountsTab;
 	private static final String USER_ID = "bb67@bb2.com", PASSWORD = "bugb1234", URL = "https://login.salesforce.com/",
-			NAME_FROM_LIST = "Adams, Bob", RECORD_TYPE = "Mass Add Promo Call", DETAILING_PRIORITY_1 = "Cholecap", DETAILING_PRIORITY_2 = "Labrinone";
+			NAME_FROM_LIST = "Adams, Bob", RECORD_TYPE = "Mass Add Promo Call";
+	private static final String[] CALL_DISCUSSION_PRODUCTS = { "Cholecap", "Labrinone" };
 
 	@BeforeTest
 	public void setup() {
-		//1. Log-in to application from a web browser (e.g. Internet Explorer, Firefox, Chrome, etc.).
+		// 1. Log-in to application from a web browser (e.g. Internet Explorer, Firefox,
+		// Chrome, etc.).
 		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -44,32 +47,47 @@ public class TestCallReport {
 
 	@Test
 	public void recordACall() {
-		//2. Navigate to "My Accounts" using tab near top of home page.
+		// 2. Navigate to "My Accounts" using tab near top of home page.
 		objAccountsTab = new MyAccountsTab(driver);
-		objAccountsTab.clickMyAccounts();		
-		//3. Select "Adams, Bob" from the My Accounts list.
+		objAccountsTab.clickMyAccounts();
+		// 3. Select "Adams, Bob" from the My Accounts list.
 		objAccountsTab.clickName(NAME_FROM_LIST);
-		//4. Near the top of the Account page is a "Record A Call" button. Select this button.
+		// 4. Near the top of the Account page is a "Record A Call" button. Select this
+		// button.
 		objAccountsTab.clickRecordACall();
-		//5. Validate the Call Report page is displayed.
+		// 5. Validate the Call Report page is displayed.
 		String actual = objAccountsTab.getFormTitle();
 		Assert.assertEquals(actual, "Call Report");
-		//6. On Call Report page, select "Mass Add Promo Call" from the Record Type drop down list.
+		// 6. On Call Report page, select "Mass Add Promo Call" from the Record Type
+		// drop down list.
 		objAccountsTab.selectRecordType(RECORD_TYPE);
-		//7. On Call Report page, the script should select Cholecap and Labrinone in Detail Priority section.
-		objAccountsTab.selectDetailingPriorityOptions(DETAILING_PRIORITY_1);
-		objAccountsTab.selectDetailingPriorityOptions(DETAILING_PRIORITY_2);
-		//8. Under Call Discussions section, make sure a subsection appeared for both Cholecap and Labrinone. Also make sure the "Product" fields are set to the respective product (one should be set to Cholecap, the other should be set to Labrinone).		
-		
-		//9. On the same Call Discussion section, make sure each section appears in order the product was selected. If Labrinone was selected first, then Labrinone should show up first (from top down) under Call Discussion section.
-		//10. In Samples and Promotional Items section, select "QNASL Co-Pay Card", and change quantity to 2.
-		//11. Call report should be saved by clicking "Save" button with a check for successful submission. 
-		//12. As final step, the script should logout (found on drop down menu in upper right
+		// 7. On Call Report page, the script should select Cholecap and Labrinone in
+		// Detail Priority section.
+		for (String product : CALL_DISCUSSION_PRODUCTS) {
+			objAccountsTab.selectDetailingPriorityOptions(product);
+		}
+		// 8. Under Call Discussions section, make sure a subsection appeared for both
+		// Cholecap and Labrinone. Also make sure the "Product" fields are set to the
+		// respective product (one should be set to Cholecap, the other should be set to
+		// Labrinone).
+		// 9. On the same Call Discussion section, make sure each section appears in
+		// order the product was selected. If Labrinone was selected first, then
+		// Labrinone should show up first (from top down) under Call Discussion section.
+		List<String> products = objAccountsTab.getCallDiscussionProducts();
+		for (int i = 0; i < products.size(); i++) {
+			Assert.assertTrue(products.get(i).contains(CALL_DISCUSSION_PRODUCTS[i]));
+		}
+		// 10. In Samples and Promotional Items section, select "QNASL Co-Pay Card", and
+		// change quantity to 2.
+		// 11. Call report should be saved by clicking "Save" button with a check for
+		// successful submission.
+		// 12. As final step, the script should logout (found on drop down menu in upper
+		// right
 	}
 
 	@AfterTest
 	public void teardown() throws InterruptedException {
-		Thread.sleep(10000);
+		Thread.sleep(5000);
 		driver.close();
 	}
 }
