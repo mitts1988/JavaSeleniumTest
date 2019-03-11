0d0f3aa010c7b7c65a806d78d4498e0ecd57a55b
mitts <mitts1988@outlook.com>
2019-03-11 16:31:04 -0400

    Finished updating login page

--- VeevaTest/src/com/selenium/pages/LoginPage.java
+++ VeevaTest/src/com/selenium/pages/LoginPage.java
@@ -1,31 +1,40 @@
 package com.selenium.pages;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 	WebDriver driver;
-	By userNameTextbox = By.id("username");
-	By passwordTextbox = By.id("password");
-	By loginButton = By.id("Login");
+	Properties selector;
 
-	public LoginPage(WebDriver driver) {
+	public LoginPage(WebDriver driver) throws IOException {
 		this.driver = driver;
+		// load properties
+		selector = new Properties();
+		FileInputStream objfile = new FileInputStream(System.getProperty("user.dir") + "\\selector.properties");
+		selector.load(objfile);
 	}
 
-	// inputs user name into textbox
+	/**
+	 * inputs user name into textbox
+	 * @param strUserName 
+	 */
 	public void inputUserName(String strUserName) {
-		driver.findElement(userNameTextbox).sendKeys(strUserName);
+		driver.findElement(By.id(selector.getProperty("username_textbox"))).sendKeys(strUserName);
 	}
 
 	// inputs password into textbox
 	public void inputPassword(String strPassword) {
-		driver.findElement(passwordTextbox).sendKeys(strPassword);
+		driver.findElement(By.id(selector.getProperty("password_textbox"))).sendKeys(strPassword);
 	}
 
 	// clicks the login button
 	public void clickLogin() {
-		driver.findElement(loginButton).click();
+		driver.findElement(By.id(selector.getProperty("login_button"))).click();
 	}
 
 	public void loginToSalesforce(String strUserName, String strPassword) {
